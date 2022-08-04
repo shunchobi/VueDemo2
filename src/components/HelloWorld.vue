1bd19154d5dd8bf11192952044c8a23c706da191
shunchobi <shunbeat1008@gmail.com>
2022-08-04 21:54:26 +0900

    commit 1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,27 +2,6 @@
 <template>
   <h1> Netlify Form and Notification Test</h1>
 
-  <form name="contact" action="/thanks.html" method="POST" netlify>
-    <p>
-      <label>Your Name: <input type="text" name="name" /></label>
-    </p>
-    <p>
-      <label>Your Email: <input type="email" name="email" /></label>
-    </p>
-    <p>
-      <label>Your Role: <select name="role[]" multiple>
-          <option value="leader">Leader</option>
-          <option value="follower">Follower</option>
-        </select></label>
-    </p>
-    <p>
-      <label>Message: <textarea name="message"></textarea></label>
-    </p>
-    <p>
-      <button type="submit">Send</button>
-    </p>
-  </form>
-
 </template>
 
 <script>
